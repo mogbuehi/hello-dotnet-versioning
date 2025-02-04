d57a8ce6905a27d6c555009e0c689436dd3cdb50
Matthew Ogbuehi <38775809+mogbuehi@users.noreply.github.com>
2025-02-04 18:45:30 +0000

    adding odd and even logic

--- hello/Program.cs
+++ hello/Program.cs
@@ -7,7 +7,12 @@ class Program
         Console.WriteLine("Hello, CS 690!");
         Console.WriteLine("Enter a number: ");
         var number = int.Parse(Console.ReadLine());
-        Console.WriteLine("This is your number: "+number);
+    
+        if(number%2 == 0){
+            Console.WriteLine(number+ " is even");
+        } else {
+            Console.WriteLine(number+ " is odd");
+        }
 
     }
 }
