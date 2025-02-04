7cdf7969b4f5ecf2f6beb06994d4778c1123087c
Matthew Ogbuehi <38775809+mogbuehi@users.noreply.github.com>
2025-02-04 18:31:38 +0000

    Ask for number and display it

--- hello/Program.cs
+++ hello/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, CS 690!");
+        Console.WriteLine("Enter a number: ");
+        var number = int.Parse(Console.ReadLine());
+        Console.WriteLine("This is your number: "+number);
+
     }
 }
